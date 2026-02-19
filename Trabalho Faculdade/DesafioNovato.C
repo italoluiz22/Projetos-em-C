40bfe3a64104b57e830d72186e68a2e1971f7c06
italoluiz22 <italoirias29@gmail.com>
2026-02-19 12:09:22 -0300

    Adicionei Meus Conhecimentos Sobre,Modificadores de Tipo,Operadores Relacionais,Operadores Logicos no Desafio Super Trunfo

--- Trabalho Faculdade/DesafioNovato.C
+++ Trabalho Faculdade/DesafioNovato.C
@@ -1,73 +1,65 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (){
-  char estado1,estado2;
-  char codigo1[10],codigo2[10];
-  char cidade1[20],cidade2[20];
-  int populaçao1,populaçao2;
-  float area1,area2;
-  float PIB1,PIB2;
-  int NPT1,NPT2;
-  float percapita1,percapita2,densidade1,densidade2 = 0;
+  char estado1, estado2;
+  char codigo1[10], codigo2[10];
+  char cidade1[20], cidade2[20];
+  unsigned long populacao1, populacao2;
+  float area1, area2;
+  float PIB1, PIB2;
+  int NPT1, NPT2;
+  float percapita1, percapita2, densidade1, densidade2;
 
  printf("Digite os dados da primeira carta:\n");
-  scanf(" %c",&estado1);
-  scanf("%s",&codigo1);
-  getchar();
-  fgets(cidade1,sizeof(cidade1),stdin);
-  scanf("%d",&populaçao1);
-  scanf("%f",&area1);
-  scanf ("%f",&PIB1);
-  scanf("%d",&NPT1);
+ scanf(" %c",&estado1);
+ scanf("%s",codigo1);
+ getchar();
+ fgets(cidade1,sizeof(cidade1),stdin);
+ cidade1[strcspn(cidade1,"\n")] = 0;
 
-  densidade1 = (float)populaçao1/area1;
-  percapita1 =PIB1/populaçao1;
+ scanf("%lu",&populacao1);
+ scanf("%f",&area1);
+ scanf("%f",&PIB1);
+ scanf("%d",&NPT1);
 
-  printf("Digite os dados da segunda carta:\n");
-  scanf(" %c",&estado2);
-  scanf("%s",&codigo2);
-  getchar();
-  fgets(cidade2,sizeof(cidade2),stdin);
-  scanf("%d",&populaçao2);
-  scanf("%f",&area2);
-  scanf ("%f",&PIB2);
-  scanf("%d",&NPT2);
+ densidade1 = populacao1 / area1;
+ percapita1 = PIB1 / populacao1;
 
-  densidade2 = (float)populaçao2/area2;
-  percapita2 =PIB2/populaçao2;
+ printf("\nDigite os dados da segunda carta:\n");
+ scanf(" %c",&estado2);
+ scanf("%s",codigo2);
+ getchar();
+ fgets(cidade2,sizeof(cidade2),stdin);
+ cidade2[strcspn(cidade2,"\n")] = 0;
 
-  
-  printf("\n");
+ scanf("%lu",&populacao2);
+ scanf("%f",&area2);
+ scanf("%f",&PIB2);
+ scanf("%d",&NPT2);
 
-printf("Carta 01 : \n");
-printf("Estado :%c\n",estado1);
-printf("Codigo :%s\n",codigo1);
-printf("Nome da cidade: %s",cidade1);
-printf("População: %d\n",populaçao1);
-printf("Area:%f km²\n",area1);
-printf("Pib:%f bilhões de reais\n",PIB1);
-printf("Número de Pontos Turísticos: %d\n",NPT1);
-printf("Densidade Populacional: %.2f\n",densidade1);
-printf("PIB per Capita: %f\n",percapita1);
+ densidade2 = populacao2 / area2;
+ percapita2 = PIB2 / populacao2;
 
-printf("\n");
+ float SuperPoder1 = populacao1 + area1 + PIB1 + NPT1 + percapita1 + densidade1;
+ float SuperPoder2 = populacao2 + area2 + PIB2 + NPT2 + percapita2 + densidade2;
 
-printf("Carta 02 : \n");
-printf("Estado :%c\n",estado2);
-printf("Codigo :%s\n",codigo2);
-printf("Nome da cidade:%s",cidade2);
-printf("População: %d\n",populaçao2);
-printf("Area:%f km²\n",area2);
-printf("Pib:%f bilhões de reais\n",PIB2);
-printf("Número de Pontos Turísticos: %d\n",NPT2);
-printf("Densidade Populacional: %.2f\n",densidade2);
-printf("PIB per Capita: %f\n",percapita2);
+ printf("\nComparação De cartas:\n");
+ printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+ printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
+ printf("PIB: Carta 1 venceu (%d)\n", PIB1 > PIB2);
+ printf("Pontos Turísticos: Carta 1 venceu (%d)\n", NPT1 > NPT2);
+ printf("Densidade: Carta 2 venceu (%d)\n", densidade1 < densidade2);
+ printf("PIB per capita: Carta 1 venceu (%d)\n", percapita1 > percapita2);
+ printf("Super Poder: Carta 1 venceu (%d)\n", SuperPoder1 > SuperPoder2);
+
+ return 0;
+}
 
 
 
 
-  return 0 ;
-};
+
 
 
 
