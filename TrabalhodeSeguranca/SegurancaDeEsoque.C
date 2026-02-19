d70d0bdb61b9de2ec287a35c76812ef8d4579623
italoluiz22 <italoirias29@gmail.com>
2026-02-19 15:09:01 -0300

    Trabalho pra colocar os conhecimento em pratica

---
+++ TrabalhodeSeguranca/SegurancaDeEsoque.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    float temperatura, umidade;
+    unsigned int estoque,estoqueMinimo = 1000;
+    
+
+
+    printf("Entre com a temperatura:  \n");
+    scanf("%f",&temperatura);
+    printf("Entre com a umidade: \n");
+    scanf("%f",&umidade);
+    printf("Entre com o estoque:  \n");
+    scanf("%u",&estoque);
+
+    if(temperatura > 30){
+        printf("Temperatura está alta\n");
+    }else{
+        printf("Temperatura está dentro dos paramentros\n");
+    }
+
+
+    if(umidade > 50){
+        printf("umidade elevada.\n");
+    }else{
+        printf("Temperatura está dentro dos paramentros\n");
+    }
+
+    if(estoque < estoqueMinimo){
+        printf("Estoque abaixo do minimo\n");
+    }else{
+        printf("Estoque normal!\n");
+
+    }
+
+    return 0;
+
+}
